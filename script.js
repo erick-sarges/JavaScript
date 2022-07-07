4ad1506b0f2a7a9bc876dc58b7aaae0a16548efd
erick <erick.sarges0223@gmail.com>
2022-07-07 14:47:05 -0300

    criacao de  sintaxe

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 
 // Nome do Aluno - nota 1 - nota 2 média -  Aprovado/ reprovado
 
+var nomes = ["Erick", "Amanda", "Diego", "Kevin","Toff"];
+
+notasA = [9.0, 9.5, 8.0, 7.5, 9.2];
+notasB = [8.0, 10, 7.6, 8.5, 6.6];
+
+function media(n1,n2){
+
+    return (n1 + n2) /2;
+
+};
+
+for(var index in nomes){
+
+    console.log(index);
+}
+
