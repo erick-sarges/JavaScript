03df5d66220029694e93888f811dc5a8978c719a
erick <erick.sarges0223@gmail.com>
2022-07-08 14:10:31 -0300

    codigo finalizado

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
 
 // Nome do Aluno - nota 1 - nota 2 média -  Aprovado/ reprovado
 
-var nomes = ["Erick", "Amanda", "Diego", "Kevin","Toff"];
+ var nomes = ["Erick","Amanda","Diego","Kevin"];
+ var notasA = [9.0, 8.0, 7.6, 7.0];
+ var notasB = [ 9.5, 9.0, 8.5, 8.0];
 
-notasA = [9.0, 9.5, 8.0, 7.5, 9.2];
-notasB = [8.0, 10, 7.6, 8.5, 6.6];
-
-function media(n1,n2){
+ function media(n1,n2){
 
     return (n1 + n2) /2;
+ };
+function passou(media){
 
+    var resultado = "Reprovado";
+
+    if( media > 7){
+
+        return "Aprovado";
+    }
+    else{
+
+        return "Reprovado";
+    }
 };
 
-for(var index in nomes){
 
-    console.log(index);
-}
+ for(index in  nomes){
+
+    var nota1 = notasA[index];
+    var nota2 = notasB[index];
+
+    var m = media(nota1 , nota2);
+
+   console.log(nomes[index] + " - " + nota1 + " - " + nota2 +  " - "+ m + " - " + passou(m));
+
+ };
 
